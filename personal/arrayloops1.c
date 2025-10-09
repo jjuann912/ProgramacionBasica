c4d7cbea9f7d5b1fa120c2d549b4f41d7d12f39d
jjuann912 <juanpablosmith1702@gmail.com>
2025-10-09 12:44:10 -0500

    Create arrayloops1.c

---
+++ personal/arrayloops1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+	int myNumbers[] = {25, 50, 75, 100};
+	int i;
+	for (i = 0; i < 4; i++) {
+	printf("%d\n", myNumbers[i]);
+}
+	return 0;
+	
+}
