3249295479358d177242788a65dfaecd58501140
jjuann912 <juanpablosmith1702@gmail.com>
2025-10-21 18:39:33 -0500

    Create main.c

---
+++ Corte1C/practica17/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int edades[] = {20, 22, 18, 35, 48, 26, 87, 70}; 
+	float avg, sum = 0; 
+	int i;
+	int length = sizeof(edades) / sizeof(edades[0]); 
+	for (i = 0; i < length; i++) { 
+  		sum += edades[i]; 
+	}
+	avg = sum / length;
+	printf("La edad promedio es igual a: %.2f", avg);	
+	return 0;
+	
+}
