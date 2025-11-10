247c4d206f407003982ebdcd2e51afbc38dd47e5
jjuann912 <juanpablosmith1702@gmail.com>
2025-11-10 14:52:56 -0500

    Create main6.c

---
+++ Corte2/C/taller/main6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int N, M, i, j;
+    printf("Ingrese filas (N): ");
+    scanf("%d", &N);
+    printf("Ingrese columnas (M): ");
+    scanf("%d", &M);
+
+    float matriz[N][M], sumaFila[N], sumaCol[M];
+    for (i = 0; i < N; i++) sumaFila[i] = 0;
+    for (j = 0; j < M; j++) sumaCol[j] = 0;
+
+    printf("Ingrese los elementos:\n");
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%f", &matriz[i][j]);
+            sumaFila[i] += matriz[i][j];
+            sumaCol[j] += matriz[i][j];
+        }
+    }
+
+    printf("\nMatriz con sumas:\n");
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++)
+            printf("%.2f\t", matriz[i][j]);
+        printf("= %.2f\n", sumaFila[i]);
+    }
+
+    for (j = 0; j < M; j++)
+        printf("%.2f\t", sumaCol[j]);
+    printf("\n");
+
+    return 0;
+}
