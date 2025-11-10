afaa5770afcb3e37c3e5cf193eb1604ad3b159d6
jjuann912 <juanpablosmith1702@gmail.com>
2025-11-10 14:51:36 -0500

    Create main1.c

---
+++ Corte2/C/taller/main1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int N, M, i, j, pares = 0;
+
+    printf("Ingrese el número de filas (N): ");
+    scanf("%d", &N);
+    printf("Ingrese el número de columnas (M): ");
+    scanf("%d", &M);
+
+    int matriz[N][M];
+
+    printf("Ingrese los elementos de la matriz:\n");
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+            if (matriz[i][j] % 2 == 0) pares++;
+        }
+    }
+
+    printf("\nLa matriz tiene %d números pares.\n", pares);
+    return 0;
+}
