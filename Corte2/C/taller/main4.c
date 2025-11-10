9aec91d153c21bd0efa6b79fc07e2e101d521348
jjuann912 <juanpablosmith1702@gmail.com>
2025-11-10 14:52:16 -0500

    Create main4.c

---
+++ Corte2/C/taller/main4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int N, i, j;
+    printf("Ingrese la dimensión N de la matriz cuadrada: ");
+    scanf("%d", &N);
+
+    int matriz[N][N], maxFila[N];
+    int sumaMax = 0;
+
+    printf("Ingrese los elementos:\n");
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    for (i = 0; i < N; i++) {
+        int max = matriz[i][0];
+        for (j = 1; j < N; j++) {
+            if (matriz[i][j] > max)
+                max = matriz[i][j];
+        }
+        maxFila[i] = max;
+        sumaMax += max;
+    }
+
+    printf("\nMáximos por fila:\n");
+    for (i = 0; i < N; i++)
+        printf("Fila %d: %d\n", i + 1, maxFila[i]);
+
+    printf("Media de los máximos: %.2f\n", (float)sumaMax / N);
+    return 0;
+}
