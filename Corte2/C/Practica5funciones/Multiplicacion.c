ba31825da3fdbd6beac636f9d535a7e29026985a
jjuann912 <juanpablosmith1702@gmail.com>
2025-11-11 19:07:57 -0500

    Update Multiplicacion.c

--- Corte2/C/Practica5funciones/Multiplicacion.c
+++ Corte2/C/Practica5funciones/Multiplicacion.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 
-int enterValue() {
-    int Value;
-    printf("Enter a number: ");
-    scanf("%d", &Value);
-    return Value;
+int ingresarValor() {
+    int valor;
+    printf("Ingresa un número: ");
+    scanf("%d", &valor);
+    return valor;
 }
 
-float divide(int a, int b) {
+float dividir(int a, int b) {
     if(b == 0) {
-        printf("Error: Can't divide between zero'\n");
+        printf("Error: No se puede dividir entre cero\n");
         return 0;
     }
     float r = (float)a / b;
     return r;
 }
 
-void showResult(float r) {
-    printf("Result: %.2f\n", r);
+void mostrarResultado(float r) {
+    printf("Resultado: %.2f\n", r);
 }
 
 int main() {
-    int operand1;
-    int operand2;
-    float result;
+    int operando1;
+    int operando2;
+    float resultado;
     
-    printf("=== DIVISION ===\n");
-    operand1 = enterValue();
-    operand2 = enterValue();
+    printf("=== DIVISIÓN ===\n");
+    operando1 = ingresarValor();
+    operando2 = ingresarValor();
     
-    result = divide(operand1, operand2);
-    if(operand2 != 0) {
-        showResult(result);
+    resultado = dividir(operando1, operando2);
+    if(operando2 != 0) {
+        mostrarResultado(resultado);
     }
     
     return 0;
